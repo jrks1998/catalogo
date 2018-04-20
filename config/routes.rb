f535aa0d1d6f72f3103db2f130ec2ca6a99ba8b7
klicheti <klicheti@gmail.com>
2018-04-19 20:10:19 -0400

    criado cadastro de grupos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :groups
+
   root 'home#index'
   get 'home/index'
   # The priority is based upon order of creation: first created -> highest priority.
